99987a36e69bf781801045542ebfef285b38e54d
Ramazan Atalay <ramazanatalay@gmail.com>
2022-01-11 14:35:52 -0500

    sh code has been removed

--- main.tf
+++ main.tf
@@ -1,62 +1,68 @@
 provider "aws" {
   region = "us-east-1"
 }
+
 variable "vpc_cidr_block" {}
 variable "subnet_cidr_block" {}
 variable "avail_zone" {}
 variable "env_prefix" {}
-variable "my_ip" {}
 variable "instance_type" {}
-variable "public_key_location" {}
+variable "ssh_key" {}
+variable "my_ip" {}
+
+data "aws_ami" "amazon-linux-image" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+output "ami_id" {
+  value = data.aws_ami.amazon-linux-image.id
+}
+
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
-    Name : "${var.env_prefix}-vpc"
+    Name = "${var.env_prefix}-vpc"
   }
 }
-resource "aws_subnet" "myapp-subnet-1" {
+
+resource "aws_subnet" "myapp-subnet" {
   vpc_id            = aws_vpc.myapp-vpc.id
   cidr_block        = var.subnet_cidr_block
   availability_zone = var.avail_zone
   tags = {
-    Name : "${var.env_prefix}-subnet-1-dev"
+    Name = "${var.env_prefix}-subnet"
   }
 }
-resource "aws_internet_gateway" "myapp-igw" {
-  vpc_id = aws_vpc.myapp-vpc.id
-  tags = {
-    Name : "${var.env_prefix}-igw"
-  }
-}
-resource "aws_route_table" "myapp-route-table" {
-  vpc_id = aws_vpc.myapp-vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.myapp-igw.id
-  }
-  tags = {
-    Name : "${var.env_prefix}-rtb"
-  }
-}
-resource "aws_route_table_association" "a-rtb-subnet" {
-  subnet_id      = aws_subnet.myapp-subnet-1.id
-  route_table_id = aws_route_table.myapp-route-table.id
-}
+
 resource "aws_security_group" "myapp-sg" {
   name   = "myapp-sg"
   vpc_id = aws_vpc.myapp-vpc.id
+
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = [var.my_ip]
   }
+
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port       = 0
     to_port         = 0
@@ -64,47 +70,60 @@ resource "aws_security_group" "myapp-sg" {
     cidr_blocks     = ["0.0.0.0/0"]
     prefix_list_ids = []
   }
+
   tags = {
-    Name : "${var.env_prefix}-sg"
+    Name = "${var.env_prefix}-sg"
   }
-}
-data "aws_ami" "latest-amazon-linux-image" {
-  most_recent = true
-  owners      = ["amazon"]
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-output "ec2_public_ip" {
-  value = aws_instance.myapp-server.public_ip
 }
 
-output "aws_ami_id" {
-  value = data.aws_ami.latest-amazon-linux-image.id
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  tags = {
+    Name = "${var.env_prefix}-internet-gateway"
+  }
+}
+
+resource "aws_route_table" "myapp-route-table" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+
+  # default route, mapping VPC CIDR block to "local", created implicitly and cannot be specified.
+
+  tags = {
+    Name = "${var.env_prefix}-route-table"
+  }
+}
+
+# Associate subnet with Route Table
+resource "aws_route_table_association" "a-rtb-subnet" {
+  subnet_id      = aws_subnet.myapp-subnet.id
+  route_table_id = aws_route_table.myapp-route-table.id
 }
 
 resource "aws_key_pair" "ssh-key" {
-  key_name   = "server-key"
-  public_key = file(var.public_key_location)
-
+  key_name   = "myapp-key"
+  public_key = file(var.ssh_key)
 }
+
+output "server-ip" {
+  value = aws_instance.myapp-server.public_ip
+}
+
 resource "aws_instance" "myapp-server" {
-  ami           = data.aws_ami.latest-amazon-linux-image.id
-  instance_type = var.instance_type
-
-  subnet_id              = aws_subnet.myapp-subnet-1.id
-  vpc_security_group_ids = [aws_security_group.myapp-sg.id]
-  availability_zone      = var.avail_zone
-
+  ami                         = data.aws_ami.amazon-linux-image.id
+  instance_type               = var.instance_type
+  key_name                    = "myapp-key"
   associate_public_ip_address = true
-  key_name                    = aws_key_pair.ssh-key.key_name
+  subnet_id                   = aws_subnet.myapp-subnet.id
+  vpc_security_group_ids      = [aws_security_group.myapp-sg.id]
+  availability_zone           = var.avail_zone
 
   tags = {
-    Name : "${var.env_prefix}-server"
+    Name = "${var.env_prefix}-server"
   }
 }
